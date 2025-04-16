1a5ccea12411e0be47b5111fa38c6393c0517b34
Abhay Panchoo <abhaypanchoo@gmail.com>
2025-04-16 17:08:18 -0400

    Update login.routes.js

--- app/routes/login.routes.js
+++ app/routes/login.routes.js
@@ -7,35 +7,45 @@ const router = express.Router()
  * @swagger
  * components:
  *   schemas:
- *     Book:
+ *     User:
  *       type: object
  *       required:
- *         - title
- *         - author
- *         - finished
+ *         - userName
+ *         - password
  *       properties:
- *         id:
+ *         userName:
  *           type: string
- *           description: The auto-generated id of the book
- *         title:
+ *           description: The username of the user
+ *         password:
  *           type: string
- *           description: The title of your book
- *         author:
- *           type: string
- *           description: The book author
- *         finished:
- *           type: boolean
- *           description: Whether you have finished reading the book
- *         createdAt:
- *           type: string
- *           format: date
- *           description: The date the book was added
+ *           description: The password of the user
  *       example:
- *         id: d5fE_asz
- *         title: The New Turing Omnibus
- *         author: Alexander K. Dewdney
- *         finished: false
- *         createdAt: 2020-03-10T04:05:06.157Z
+ *         userName: test
+ *         password : test
+ */
+
+/**
+ * @swagger
+ * /login/api:
+ *   post:
+ *     summary: Login as a user to get auth token
+ *     tags: [User]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/x-www-form-urlencoded:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: The api authentication token
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       500:
+ *         description: Some server error
+ *
  */
 router.post('/', loginMiddleware, LoginController.loginWebsite)
 
